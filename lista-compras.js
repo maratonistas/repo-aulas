7f4e331b96bba39a36874dac4419e7f8f35d732b
C1298955 Matheus Fernandes de Jesus <c1298955@interno.bb.com.br>
2020-10-07 20:39:47 -0300

    Codigo até a pausa

--- lista-compras.js
+++ lista-compras.js
@@ -4,13 +4,19 @@
     // MODULO
     angular.module('listaTarefasApp', []);
 
+    angular.module('listaTarefasApp')
+        .filter('capitalize', function () {
+            return function (input) { return input.charAt(0).toUpperCase() + input.substring(1); }
+        });
+
+
     // CONTROLLER
     angular.module('listaTarefasApp')
         .controller('ListaTarefasController', listaTarefasController);
 
-    listaTarefasController.$inject = [];
+    listaTarefasController.$inject = ['$rootScope'];
 
-    function listaTarefasController() {
+    function listaTarefasController(root) {
         var vm = this;
         /* ***************    INIT VARIÁVEIS    *********************************** */
         vm.logado = false;
@@ -18,6 +24,8 @@
         vm.register = false;
 
         /* ***************    FUNÇÕES EXECUTADAS NA VIEW (HMTL)    **************** */
+        root.listaMensagens = [{ text: 'TESTE MENSAGEM.. teste', tipo: 'success' }];
+        vm.teste = 'teste';
 
         /* ***************    FUNÇÕES INSTERNAS    ******************************** */
 

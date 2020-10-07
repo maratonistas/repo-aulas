13e79fb2b921b34fe869c3153152b0d470eab470
C1298955 Matheus Fernandes de Jesus <c1298955@interno.bb.com.br>
2020-10-07 17:49:55 -0300

    Subindo projeto base

---
+++ lista-compras.js
@@ -0,0 +1,26 @@
+(function (angular) {
+    "use strict";
+
+    // MODULO
+    angular.module('listaTarefasApp', []);
+
+    // CONTROLLER
+    angular.module('listaTarefasApp')
+        .controller('ListaTarefasController', listaTarefasController);
+
+    listaTarefasController.$inject = [];
+
+    function listaTarefasController() {
+        var vm = this;
+        /* ***************    INIT VARIÁVEIS    *********************************** */
+        vm.logado = false;
+        vm.validar = false;
+        vm.register = false;
+
+        /* ***************    FUNÇÕES EXECUTADAS NA VIEW (HMTL)    **************** */
+
+        /* ***************    FUNÇÕES INSTERNAS    ******************************** */
+
+    }
+
+})(angular);
